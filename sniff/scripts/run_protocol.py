7ca74887368a89f6cdc70a3a6143038526d7354f
Ian Snider <iqsnider@gmail.com>
2025-10-29 09:38:35 -0700

    feat: follows dynamic setpoint

--- sniff/scripts/run_protocol.py
+++ sniff/scripts/run_protocol.py
@@ -90,5 +90,37 @@ def formation(
     plotter.plot_2D_paths(p0, save)
 
 
+@app.command()
+def circle(
+    n: int = 5,
+    link: float = 1.0,
+    time: float = 10.0,
+    time_step: float = 0.01,
+    alpha: float = 1.0,
+    beta: float = 1.0,
+    noise_strength: float = 0.1,
+    noise_model: str = 'piecewise',
+    noise_update_rate: float = 0.1,
+    spacing: float = 0.05,
+    center: tuple[float, float] = (0.0, 0.0),
+    radius: float = 0.5,
+    ang_vel: float = 1.0,
+    save: str = None
+):
+    graph = Graph(n)
+    A = graph.erdos_renyi_adj(link)
+
+    x0 = np.random.random((n, 2))
+    p0 = x0.flatten()
+
+    simulation = Simulate(A, time, time_step, p0,
+                          noise_model, noise_update_rate)
+    logs = simulation.solve_consensus_formation_circle_tracking_w_GOE_noise(
+        alpha=alpha, beta=beta, center=center, radius=radius, ang_vel=ang_vel, noise_strength=noise_strength, spacing=spacing)
+
+    plotter = Plot(logs)
+    plotter.plot_2D_paths(p0, save)
+
+
 if __name__ == "__main__":
     app()
